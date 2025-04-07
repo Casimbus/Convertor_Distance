f98e8c6c8f848a4ab5244ddb69cd4ca1ee8d70c5
Casimbus <danila.id17@gmail.com>
2025-04-07 15:02:39 +0200

    Update app - added converting to kilometers

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,7 +1,16 @@
 package app;
 
 public class Main {
+
+    private static final double CONV_K = 1.60934;
+
+    public static double convMilesToKm(double miles){
+        return miles * CONV_K;
+    }
     public static void main(String[] args) {
+        double mls = 5;
+        double kilometers = convMilesToKm(mls);
         System.out.println("This is distance converting app.\nVersion 1.0");
+        System.out.printf("The result is: %.3f km.",kilometers);
     }
 }
